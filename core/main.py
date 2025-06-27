d935dc4377639b72129b718bcbe0b34f4694ee15
kaizensaito <kaizen.saito.ai@gmail.com>
2025-06-27 18:32:27 -0300

    Update main.py

--- core/main.py
+++ core/main.py
@@ -1,8 +1,11 @@
 import sys, os
-sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 
 from core.router import app
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
