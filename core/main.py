3ca455cd2288138f169b78f1bd10032dca051f0e
kaizensaito <kaizen.saito.ai@gmail.com>
2025-06-27 20:50:05 -0300

    Update main.py

--- core/main.py
+++ core/main.py
@@ -1,8 +1,4 @@
-from flask import Flask
-from core.router import router
-
-app = Flask(__name__)
-app.register_blueprint(router)
+from core.router import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
