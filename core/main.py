f1250800aab08c6c2d121af63a03e323df9ba175
kaizensaito <kaizen.saito.ai@gmail.com>
2025-06-27 18:20:50 -0300

    Update main.py

--- core/main.py
+++ core/main.py
@@ -1,9 +1,8 @@
 import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
 from core.router import app
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
